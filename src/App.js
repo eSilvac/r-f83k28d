1d5f3a87029b770096d0fa82cfc174b56802fabe
esilvac <esilva1995@gmail.com>
2019-10-22 13:50:09 -0500

    Solve counter

--- src/App.js
+++ src/App.js
@@ -2,11 +2,26 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.addCount = this.addCount.bind(this);
+    this.state = {
+      count: 0
+    }
+  }
+
+  addCount() {
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
+
   render() {
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">{this.state.count}</span>
+        <button id="inc" onClick={this.addCount}>Incrementa</button>
       </div>
     );
   }
